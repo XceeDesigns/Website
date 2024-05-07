c9114cfe2e9434d68a32a19b44badde5005ad570
HxrshB <bharsh.shg@gmail.com>
2024-05-07 13:35:17 +0530

    Segregated script file from contact dashboard.

---
+++ contact-dashboard.js
@@ -0,0 +1,95 @@
+
+        const fetchData = async () => {
+            const input = document.getElementById('fetchInput').value;
+            const authToken = await fetch('https://xceedesigns-backend.vercel.app/contact/admin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ adminAuth: input })
+
+            });
+            const authTokenData = await authToken.text();
+            if (authTokenData != 'Admin Authenticated') {
+                const modalTitle = document.querySelector('.modal-title');
+                modalTitle.textContent = 'XceeDesigns LLC.';
+                modalTitle.style.fontWeight = 'bold';
+                const modalBody = document.querySelector('.modal-body');
+                modalBody.textContent = 'Authentication failed, please try again.';
+                $('#myModal').modal('show');
+                return;
+            }
+            const response = await fetch(`https://xceedesigns-backend.vercel.app/contact/fetch`);
+            const data = await response.json();
+
+            const table = document.createElement('table');
+            table.classList.add('table');
+
+            // Remove previous table if exists
+            const previousTable = document.querySelector('table');
+            if (previousTable) {
+                previousTable.remove();
+            }
+
+            const tableHead = document.createElement('thead');
+            const tableHeadRow = document.createElement('tr');
+            const nameHeader = document.createElement('th');
+            nameHeader.textContent = 'Name';
+            tableHeadRow.appendChild(nameHeader);
+            const emailHeader = document.createElement('th');
+            emailHeader.textContent = 'Email';
+            tableHeadRow.appendChild(emailHeader);
+            const messageHeader = document.createElement('th');
+            messageHeader.textContent = 'Message';
+            tableHeadRow.appendChild(messageHeader);
+            const deleteHeader = document.createElement('th');
+            deleteHeader.textContent = 'Delete';
+            tableHeadRow.appendChild(deleteHeader);
+            tableHead.appendChild(tableHeadRow);
+            table.appendChild(tableHead);
+
+            const tableBody = document.createElement('tbody');
+            data.forEach(entry => {
+                const tableRow = document.createElement('tr');
+
+                const nameCell = document.createElement('td');
+                nameCell.textContent = entry.name;
+                tableRow.appendChild(nameCell);
+
+                const emailCell = document.createElement('td');
+                emailCell.textContent = entry.email;
+                tableRow.appendChild(emailCell);
+
+                const messageCell = document.createElement('td');
+                messageCell.textContent = entry.message;
+                messageCell.style.maxWidth = '300px';
+                messageCell.style.maxHeight = '100px'; // Set a maximum height for the message cell
+                messageCell.style.wordWrap = 'break-word'; // Wrap long words
+                messageCell.style.whiteSpace = 'normal';
+                tableRow.appendChild(messageCell);
+
+                const deleteCell = document.createElement('td');
+                const deleteIcon = document.createElement('div');
+                deleteIcon.classList.add('delete-icon');
+                deleteIcon.textContent = '🗑️';
+                deleteIcon.style.cursor = 'pointer';
+                deleteIcon.addEventListener('click', async () => {
+                    const res = await fetch(`https://xceedesigns-backend.vercel.app/contact/delete/${entry._id}`, {
+                        method: 'DELETE'
+                    });
+                    tableRow.remove();
+
+                });
+                deleteCell.appendChild(deleteIcon);
+                tableRow.appendChild(deleteCell);
+
+                tableBody.appendChild(tableRow);
+            });
+            table.appendChild(tableBody);
+
+            const tableContainer = document.createElement('div');
+            tableContainer.classList.add('table-responsive');
+            tableContainer.appendChild(table);
+
+            document.getElementById('dashboard').appendChild(tableContainer);
+        };
