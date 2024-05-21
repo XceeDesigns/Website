31dca53d9f4d444b82f79a2532a9442e44fb9384
Sailesh <chandrasailesh30@gmail.com>
2024-05-21 17:17:30 +0530

    added validation

--- contact.js
+++ contact.js
@@ -5,6 +5,14 @@
       const contactSubmit = document.getElementById('contact-submit');
       contactForm.addEventListener('submit', async function (event) {
         event.preventDefault();
+        const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const emailInput = document.getElementById('email').value;
+  
+        // Check if email matches the pattern
+        if (!emailPattern.test(emailInput)) {
+          alert('Please enter a valid email address (e.g., user@example.com)');
+          return;
+        }
           // sending data back to API
         const response = await fetch('https://xceedesigns-backend.vercel.app/contact', {
           method: 'POST',
