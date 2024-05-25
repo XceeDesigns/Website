9c03f0b97dc142cc40c672871bea94004200c353
Aish Mishra <aishlunatic001@gmail.com>
2024-05-25 21:10:59 +0530

    hamburger icon issue solved

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import logo from '../assets/Images/logo-new.png'
+import { IoMenu } from "react-icons/io5";
+import '../Navbar.css/bootstrap.min.css'
+import '../Navbar.css/icons-fonts.css'
+import '../Navbar.css/style.css'
 
 
 function Navbar() {
@@ -33,10 +37,13 @@ function Navbar() {
                     data-toggle="collapse"
                     data-target="#main-menu .navbar-collapse"
                   >
-                    <span
-                      aria-hidden="true"
-                      className="icon_menu hamb-mob-icon"
-                    ></span>
+                     <IoMenu
+                  style={{
+                    fontSize : '1.9em' , 
+                    marginRight : '10px',
+                    marginTop : '22px', 
+                  }}
+                />
                   </button>
                 </div>
               </div>
@@ -96,144 +103,7 @@ function Navbar() {
             </div>
           </div>
         </header>
-
-        <div id="index-link" className="relative">
-          <div
-            className="rev_slider_wrapper fullscreen-container"
-            id="rev_slider_280_1_wrapper"
-            style={{ backgroundColor: "#fff", padding: "0px" }}
-          >
-            <div
-              className="rev_slider fullscreenbanner"
-              data-version="5.1.4"
-              id="rev_slider_280_1"
-              style={{ display: "none" }}
-            >
-              <ul>
-                <li
-                  data-description=""
-                  data-easein="Power2.easeInOut"
-                  data-easeout="default"
-                  data-index="rs-898"
-                  data-masterspeed="1500"
-                  data-param1=""
-                  data-param10=""
-                  data-param2=""
-                  data-param3=""
-                  data-param4=""
-                  data-param5=""
-                  data-param6=""
-                  data-param7=""
-                  data-param8=""
-                  data-param9=""
-                  data-rotate="0"
-                  data-saveperformance="off"
-                  data-slotamount="default"
-                  data-title="Slide"
-                  data-transition="fade"
-                >
-                  <img
-                    alt="img"
-                    className="rev-slidebg"
-                    data-bgparallax="12"
-                    data-bgposition="top center"
-                    data-duration="30000"
-                    data-ease="Linear.easeNone"
-                    data-no-retina=""
-                    src="images/revo-slider/index-portfolio2.jpg"
-                  />
-
-                  <div
-                    className="tp-caption font-lato font-white tp-resizeme"
-                    id="slide-898-layer-1"
-                    style={{
-                      zIndex: 8,
-                      whiteSpace: "nowrap",
-                      letterSpacing: "10px",
-                    }}
-                    data-fontsize='["77","77","52","49"]'
-                    data-fontweight="300"
-                    data-height="none"
-                    data-lineheight='["85","85","85","85"]'
-                    data-responsive_offset="on"
-                    data-splitin="none"
-                    data-splitout="none"
-                    data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:.95;","delay":500,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"nothing"}]'
-                    data-whitespace="nowrap"
-                    data-width="none"
-                    data-x='["center","center","center","center"]'
-                    data-hoffset='["0","0","0","0"]'
-                    data-y='["center","center","center","center"]'
-                    data-voffset='["-105","-105","-90","-85"]'
-                  >
-                    IMAGINATION
-                  </div>
-
-                  <div
-                    className="tp-caption font-lato font-white tp-resizeme"
-                    id="slide-898-layer-2"
-                    style={{
-                      zIndex: 8,
-                      whiteSpace: "nowrap",
-                      letterSpacing: "10px",
-                    }}
-                    data-fontsize='["67","67","45","42"]'
-                    data-fontweight="300"
-                    data-height="none"
-                    data-lineheight='["80","80","80","80"]'
-                    data-responsive_offset="on"
-                    data-splitin="none"
-                    data-splitout="none"
-                    data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:.95;","delay":900,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"nothing"}]'
-                    data-whitespace="nowrap"
-                    data-width="none"
-                    data-x='["center","center","center","center"]'
-                    data-hoffset='["0","0","0","0"]'
-                    data-y='["center","center","center","center"]'
-                    data-voffset='["-20","-20","-20","-20"]'
-                  >
-                    WILL TAKE YOU
-                  </div>
-
-                  <div
-                    className="tp-caption font-lato font-white tp-resizeme"
-                    id="slide-898-layer-3"
-                    style={{
-                      zIndex: 8,
-                      whiteSpace: "nowrap",
-                      letterSpacing: "10px",
-                    }}
-                    data-fontsize='["78","78","52","50"]'
-                    data-fontweight="600"
-                    data-height="none"
-                    data-lineheight='["90","90","90","90"]'
-                    data-responsive_offset="on"
-                    data-splitin="none"
-                    data-splitout="none"
-                    data-frames='[{"from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","speed":1500,"to":"o:.95;","delay":1300,"ease":"Power3.easeInOut"},{"delay":"wait","speed":1000,"to":"y:[100%];","mask":"x:inherit;y:inherit;s:inherit;e:inherit;","ease":"nothing"}]'
-                    data-whitespace="nowrap"
-                    data-width="none"
-                    data-x='["center","center","center","center"]'
-                    data-hoffset='["0","0","0","0"]'
-                    data-y='["center","center","center","center"]'
-                    data-voffset='["70","70","55","50"]'
-                  >
-                    EVERYWHERE
-                  </div>
-                </li>
-              </ul>
-              <div
-                className="tp-bannertimer tp-bottom"
-                style={{ visibility: "hidden !important" }}
-              ></div>
-            </div>
-          </div>
-          <div className="local-scroll-cont font-white">
-            <a href="#about" className="scroll-down smooth-scroll">
-              <div className="icon icon-arrows-down"></div>
-            </a>
-          </div>
-        </div>
+        
       </div>
     </>
   );
